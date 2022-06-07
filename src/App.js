ad5f70ddd330d2aa8c744f9993ec71868d2fdc92
moohabir <87937848+moohabir@users.noreply.github.com>
2022-06-07 22:54:51 +0200

    sas

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import axios from "axios";
 
 export default function App() {
   const [container, setContainer] = useState([]);
+  const [query, setQuery] = useState("");
 
   useEffect(() => {
     const options = {
       method: "GET",
       url: "https://edamam-food-and-grocery-database.p.rapidapi.com/parser",
-      params: { ingr: "apple" },
+      params: { ingr: +{ query } },
       headers: {
         "X-RapidAPI-Host": "edamam-food-and-grocery-database.p.rapidapi.com",
         "X-RapidAPI-Key": "bc8006d48amsh69e5064484e98b7p16d73bjsn37edd8bac393"
@@ -25,18 +26,38 @@ export default function App() {
       .catch(function (error) {
         console.error(error);
       });
-  }, []);
+  }, [query]);
 
   const conts = container?.slice(0, 7);
+
+  function submitHandler(event) {
+    event.preventDefault();
+    setQuery(query);
+  }
 
   return (
     <div className="App">
       <div className="app-list">
+        <form onSubmit={submitHandler}>
+          <input
+            type="text"
+            value={query}
+            onChange={(event) => setQuery(event.target.value)}
+          />
+          <button type="submit">Search</button>
+        </form>
         {conts.map((item) => (
           <div className="items">
             <img src={item.food.image} alt="" />
             <h1>{item.food.label}</h1>
-            <p>{item.food.nutrients.FAT}</p>
+            <p>{item.food.foodContentsLabel}</p>
+            <p>
+              ingredierents: fat: {item.food.nutrients.FAT}
+              <span> CHOCDF: {item.food.nutrients.CHOCDF}</span>
+              <span> Energy: {item.food.nutrients.ENERC_KCAL}</span>
+              <span> fibbtg: {item.food.nutrients.FIBTG}</span>
+              <span> PROCNT :{item.food.nutrients.PROCNT}</span>
+            </p>
           </div>
         ))}
       </div>
